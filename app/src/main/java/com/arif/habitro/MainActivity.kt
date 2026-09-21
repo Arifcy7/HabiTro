c24ae419e3a689db5e13b8dce88a4b3c1d3f3460
Arifcy7 <choudharyarif756@gmail.com>
2026-09-21 12:49:09 +0530

    Initial commit

---
+++ app/src/main/java/com/arif/habitro/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.arif.habitro
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.arif.habitro.ui.theme.HabiTroTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HabiTroTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
+
